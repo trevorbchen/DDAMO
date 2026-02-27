490206ef2ded328b7858ec7eb4adc5776b796122
jsunn-y <jasonyang12345@gmail.com>
2026-02-27 14:27:44 -0800

    flashinity is working now

--- scripts/exps/denovo/test.sh
+++ scripts/exps/denovo/test.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python run.py sampler=standard reward=none
+CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python run.py sampler=uncond reward=none
 
 CUDA_VISIBLE_DEVICES=1 HYDRA_FULL_ERROR=1 python run.py sampler=daps reward=mw
